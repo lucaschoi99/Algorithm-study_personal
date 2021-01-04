bcddf9c50caf0ca4e2a6b215495d70c92abb6187
lucaschoi99 <lucas0209@yonsei.ac.kr>
2021-01-05 05:15:58 +0900

    MST 원리 연습

---
+++ algorithm study(2020_winter)/9372.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int m, n;
+        cin >> m >> n;
+        for (int s = 0; s < n; s++) {
+            int a, b;
+            cin >> a >> b;
+        }
+        cout << m - 1 << endl;
+    }
+
+
+    return 0;
+}
