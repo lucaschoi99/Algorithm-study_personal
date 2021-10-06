6801ca649cec5cfd236fed94a7d72ba007666cba
lucaschoi99 <lucas0209@yonsei.ac.kr>
2021-10-07 01:47:17 +0900

    자료형 return값 조심하기

---
+++ algorithm study(2021)/위클리챌린지#부족한금액.cpp
@@ -0,0 +1,18 @@
+using namespace std;
+
+long long solution(int price, int money, int count)
+{
+    long long answer = -1;
+    long long sum = 0;
+    for(int i=1;i<=count;i++){
+        sum += price*i;
+    }
+    answer = sum - money;
+    if(answer > 0){
+        return answer;
+    }
+    else{
+        return 0;
+    }
+
+}
