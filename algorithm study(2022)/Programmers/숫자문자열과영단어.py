adb3cfd257966dd332e7a38e5183d07db0bf1d97
Minsoo Choi <73485743+lucaschoi99@users.noreply.github.com>
2022-10-20 21:49:36 +0900

    개선 풀이: 딕셔너리에서의 iter -> str.replace 방법

--- algorithm study(2022)/Programmers/숫자문자열과영단어.py
+++ algorithm study(2022)/Programmers/숫자문자열과영단어.py
@@ -27,3 +27,14 @@ def solution(s):
                 solve(words, alphabets)
     
     return int(answer)
+
+---------------------------------------------------------------------
+(개선 풀이)
+def solution(s):
+    answer = s
+    str_dict = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+    
+    for key, values in str_dict.items():
+        answer = answer.replace(key, values)
+    
+    return int(answer)
